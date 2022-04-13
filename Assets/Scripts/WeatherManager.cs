57f9e12f42982a4677df990392eaaf3a55f7e7ab
Iain McManus <imcmanus@injaia.com>
2022-04-13 18:15:21 +1000

    Part 2 Adding Presets

--- Assets/Scripts/WeatherManager.cs
+++ Assets/Scripts/WeatherManager.cs
@@ -7,6 +7,62 @@ using UnityEngine.Rendering.HighDefinition;
 
 public class WeatherManager : MonoBehaviour
 {
+    public class WeatherState
+    {
+        public float InitialIntensity;
+        public float CurrentIntensity;
+        public float TargetIntensity;
+
+        bool TransitionInProgress = false;
+        float TransitionProgress;
+        float TransitionTime;
+        WeatherElementConfig Config;
+
+        public void SwitchToNewPreset(WeatherElementConfig config, float transitionTime)
+        {
+            // transfer the previous intensity as our starting point
+            InitialIntensity = CurrentIntensity;
+
+            // pick a new intensity
+            TargetIntensity = config.GetRandomIntensity();
+
+            // store the config
+            Config = config;
+
+            // setup the transition
+            TransitionProgress = transitionTime > 0f ? 0f : 1f;
+            TransitionTime = transitionTime;
+            TransitionInProgress = true;
+        }
+
+        public float Tick()
+        {
+            // if there's no transition then do nothing
+            if (!TransitionInProgress)
+                return CurrentIntensity;
+
+            // need to update progress?
+            if (TransitionProgress < 1f)
+                TransitionProgress += Time.deltaTime / TransitionTime;
+
+            // update intensity?
+            CurrentIntensity = Mathf.Lerp(InitialIntensity, TargetIntensity, TransitionProgress);
+
+            // transition finished?
+            if (TransitionProgress >= 1f)
+            {
+                InitialIntensity = CurrentIntensity;
+                TargetIntensity = Config.GetRandomIntensity();
+                TransitionProgress = 0f;
+                TransitionTime = Config.GetFluctuationTime();
+
+                TransitionInProgress = TransitionTime > 0f;
+            }
+
+            return CurrentIntensity;
+        }
+    }
+
     [SerializeField, Range(0f, 1f)] float RainIntensity;
     [SerializeField, Range(0f, 1f)] float SnowIntensity;
     [SerializeField, Range(0f, 1f)] float HailIntensity;
@@ -20,11 +76,33 @@ public class WeatherManager : MonoBehaviour
     [SerializeField] VisualEffect HailVFX;
     [SerializeField] Volume FogVolume;
 
+    [Header("Default Preset")]
+    [SerializeField] WeatherPreset DefaultWeather;
+
+    [Header("Transition Debug")]
+    [SerializeField] bool DEBUG_PerformTransition;
+    [SerializeField] WeatherPreset DEBUG_TargetPreset;
+    [SerializeField] float DEBUG_TransitionTime;
+
     float PreviousRainIntensity;
     float PreviousHailIntensity;
     float PreviousSnowIntensity;
     float PreviousFogIntensity;
     Fog CachedFogComponent;
+
+    WeatherPreset CurrentWeather;
+
+    WeatherState State_Rain = new WeatherState();
+    WeatherState State_Hail = new WeatherState();
+    WeatherState State_Snow = new WeatherState();
+    WeatherState State_Fog = new WeatherState();
+
+    float InitialFluctuation = 0f;
+    public float CurrentFluctuation = 0f;
+    float TargetFluctuation = 0f;
+    float FluctuationTime = 0f;
+    float FluctuationProgress = 0f;
+    bool FluctuationInProgress = false;
 
     // Start is called before the first frame update
     void Start()
@@ -42,11 +120,21 @@ public class WeatherManager : MonoBehaviour
                                                                MinFogAttenuationDistance,
                                                                FogIntensity));
         }
+
+        ChangeWeather(DefaultWeather, 0f);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (DEBUG_PerformTransition)
+        {
+            DEBUG_PerformTransition = false;
+            ChangeWeather(DEBUG_TargetPreset, DEBUG_TransitionTime);
+        }
+
+        Update_WeatherTransition();
+
         if (RainIntensity != PreviousRainIntensity)
         {
             PreviousRainIntensity = RainIntensity;
@@ -72,5 +160,51 @@ public class WeatherManager : MonoBehaviour
                                                                    MinFogAttenuationDistance,
                                                                    FogIntensity);
         }
+    }
+
+    void Update_WeatherTransition()
+    {
+        // is there a fluctuation?
+        if (FluctuationInProgress)
+        {
+            FluctuationProgress += Time.deltaTime / FluctuationTime;
+
+            CurrentFluctuation = Mathf.Lerp(InitialFluctuation, TargetFluctuation, FluctuationProgress);
+
+            // fluctuation finished? start a new one
+            if (FluctuationProgress >= 1f)
+            {
+                InitialFluctuation = CurrentFluctuation;
+                TargetFluctuation = CurrentWeather.GetRandomFluctuation();
+                FluctuationTime = CurrentWeather.GetFluctuationTime();
+                FluctuationProgress = 0f;
+            }
+        }
+        
+        RainIntensity = Mathf.Clamp01(CurrentFluctuation + State_Rain.Tick());
+        HailIntensity = Mathf.Clamp01(CurrentFluctuation + State_Hail.Tick());
+        SnowIntensity = Mathf.Clamp01(CurrentFluctuation + State_Snow.Tick());
+        FogIntensity  = Mathf.Clamp01(CurrentFluctuation + State_Fog.Tick());
+    }
+
+    public void ChangeWeather(WeatherPreset newWeather, float transitionTime)
+    {
+        CurrentWeather = newWeather;
+
+        State_Rain.SwitchToNewPreset(newWeather.Rain, transitionTime);
+        State_Hail.SwitchToNewPreset(newWeather.Hail, transitionTime);
+        State_Snow.SwitchToNewPreset(newWeather.Snow, transitionTime);
+        State_Fog.SwitchToNewPreset(newWeather.Fog, transitionTime);
+
+        // setup for the fluctuation
+        InitialFluctuation = CurrentFluctuation;
+        TargetFluctuation = CurrentWeather.GetRandomFluctuation();
+        FluctuationTime = CurrentWeather.GetFluctuationTime();
+        FluctuationProgress = 0f;
+        FluctuationInProgress = FluctuationTime > 0f;
+
+        // no fluctuation happening - reset the modifiers
+        if (!FluctuationInProgress)
+            CurrentFluctuation = InitialFluctuation = TargetFluctuation = 0f;
     }
 }
---
+++ Assets/Scripts/WeatherPreset.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class WeatherElementConfig
+{
+    [Range(0f, 1f)] public float Intensity = 0f;
+
+    [Range(0f, 1f)] public float FluctuationAmount = 0f;
+    public float MinFluctuationInterval = 0f;
+    public float MaxFluctuationInterval = 0f;
+
+    public float GetRandomIntensity()
+    {
+        if (Intensity <= 0f)
+            return 0f;
+
+        return Mathf.Clamp01(Intensity + Random.Range(-FluctuationAmount, FluctuationAmount));
+    }
+
+    public float GetFluctuationTime()
+    {
+        return Random.Range(MinFluctuationInterval, MaxFluctuationInterval);
+    }
+}
+
+[CreateAssetMenu(menuName = "Weather/Preset", fileName = "WeatherPreset")]
+public class WeatherPreset : ScriptableObject
+{
+    [Header("Individual Effects")]
+    public WeatherElementConfig Rain;
+    public WeatherElementConfig Hail;
+    public WeatherElementConfig Snow;
+    public WeatherElementConfig Fog;
+
+    [Header("Overall")]
+    [Range(0f, 1f)] public float FluctuationAmount = 0f;
+    public float MinFluctuationInterval = 0f;
+    public float MaxFluctuationInterval = 0f;
+
+    public float GetFluctuationTime()
+    {
+        return Random.Range(MinFluctuationInterval, MaxFluctuationInterval);
+    }
+    public float GetRandomFluctuation()
+    {
+        if (FluctuationAmount <= 0f)
+            return 0f;
+
+        return Random.Range(-FluctuationAmount, FluctuationAmount);
+    }
+}
